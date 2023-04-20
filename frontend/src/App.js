ff4c4d21995933da234b46ee3a568b475d1f3bd8
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-04-20 20:05:44 +0200

    Add frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import React from "react";
+import "./styles/main.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">React</div>;
 }
 
 export default App;
